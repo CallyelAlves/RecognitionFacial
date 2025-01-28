09a19485f110bb1f9048c5c7c5c49aca0c6e5545
Callyel <callyelalves@hotmail.com>
2025-01-28 09:59:37 -0300

    add plugin.xml

--- www/recognitionFacial.js
+++ www/recognitionFacial.js
@@ -1,8 +1,10 @@
 var recognitionFacial = {
     enrollFromBase64: function(personId, image, successCallback, errorCallback) {
+        console.log("Calling enrollFromBase64 with personId:", personId);
         cordova.exec(successCallback, errorCallback, "RecognitionFacial", "enrollFromBase64", [personId, image]);
     },
     initialize: function(successCallback, errorCallback) {
+        console.log("Calling initialize");
         cordova.exec(successCallback, errorCallback, "RecognitionFacial", "initialize", []);
     }
 };
