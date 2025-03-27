8edd28c0293d6e0d5bb7456db3c0064f3f2c9771
Callyel <callyelalves@hotmail.com>
2025-03-27 10:46:02 -0300

    ajuste neurotechnology.js

--- www/neurotechnology.js
+++ www/neurotechnology.js
@@ -1,24 +1,24 @@
-var recognitionFacial = {
+var neurotechnology = {
     initialize: function(successCallback, errorCallback) {
         console.log("Calling initialize");
-        cordova.exec(successCallback, errorCallback, "RecognitionFacial", "initialize", []);
+        cordova.exec(successCallback, errorCallback, "Neurotechnology", "initialize", []);
     },
     initializeMatchingClient: function(successCallback, errorCallback) {
         console.log("Calling initialize");
-        cordova.exec(successCallback, errorCallback, "RecognitionFacial", "initializeMatchingClient", []);
+        cordova.exec(successCallback, errorCallback, "Neurotechnology", "initializeMatchingClient", []);
     },
     enrollFromBase64: function(personId, image, successCallback, errorCallback) {
         console.log("Calling enrollFromBase64 with personId:", personId);
-        cordova.exec(successCallback, errorCallback, "RecognitionFacial", "enrollFromBase64", [personId, image]);
+        cordova.exec(successCallback, errorCallback, "Neurotechnology", "enrollFromBase64", [personId, image]);
     },
     identifyBase64: function(image, successCallback, errorCallback) {
         console.log("Calling identifyBase64 with image.");
-        cordova.exec(successCallback, errorCallback, "RecognitionFacial", "identifyBase64", [image]);
+        cordova.exec(successCallback, errorCallback, "Neurotechnology", "identifyBase64", [image]);
     },
     startCamera: function(successCallback, errorCallback) {
         console.log("Calling startCamera with image.");
-        cordova.exec(successCallback, errorCallback, 'RecognitionFacial', 'startCamera', []);
+        cordova.exec(successCallback, errorCallback, 'Neurotechnology', 'startCamera', []);
     }
 };
 
-module.exports = recognitionFacial;
+module.exports = neurotechnology;
