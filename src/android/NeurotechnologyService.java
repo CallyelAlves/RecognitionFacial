13836f31c0fd6686a8a6f2c3fb114bbe55134b26
Callyel Alves <callyelalves@nasajon.com.br>
2025-10-03 11:54:18 -0300

    Revert "ajuste na quebra do build pos atualização do sdk"
    This reverts commit 8681a99944ebaf2245059686ae2ef873ae585b45.

--- src/android/NeurotechnologyService.java
+++ src/android/NeurotechnologyService.java
@@ -84,7 +84,6 @@ import com.neurotec.biometrics.NLivenessMode;
 import com.neurotec.biometrics.NMatchingResult;
 import com.neurotec.biometrics.NSubject;
 import com.neurotec.biometrics.NTemplateSize;
-import com.neurotec.geometry.NRect;
 import com.neurotec.biometrics.NBiometricCaptureOption;
 import com.neurotec.images.NImage;
 import com.neurotec.images.NImageFormat;
@@ -1190,13 +1189,7 @@ public class NeurotechnologyService implements LicensingManager.LicensingStateCa
 
                             if (!detectedFace.getObjects().isEmpty() && (score > livenessScore)) {
                                 NLAttributes faceAttributes = detectedFace.getObjects().get(0);
-                                NRect nRect = faceAttributes.getBoundingRect();
-                                Rect boundingRect = new Rect(
-                                    nRect.left,
-                                    nRect.top,
-                                    nRect.right,
-                                    nRect.bottom
-                                );
+                                Rect boundingRect = faceAttributes.getBoundingRect();
                                 int faceWidth = boundingRect.width();
                                 int imageWidth = image.getWidth();
 
